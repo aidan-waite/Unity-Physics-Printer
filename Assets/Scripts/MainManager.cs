82158b216ea0b89335bd94614ef7b0c69dfa8957
Aidan Waite <aidanaidan@gmail.com>
2021-02-19 16:55:33 +0900

    Bare bones input image, paint, and save image

---
+++ Assets/Scripts/MainManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using SFB;
+using TMPro;
+using System;
+
+public class MainManager : MonoBehaviour
+{
+    public Texture2D InputImageTexture;
+    public TextMeshProUGUI InputFileText;
+    public TextMeshProUGUI OutputFolderText;
+    public TextMeshProUGUI FeedbackText;
+    public GameObject Canvas;
+
+    ExtensionFilter[] extensions = new [] {
+        new ExtensionFilter("Image Files", "png", "jpg", "jpeg" ),
+        new ExtensionFilter("All Files", "*" ),
+    };
+
+    void Start() {
+        FeedbackText.text = "";
+    }
+
+    public void DidTapSelectSaveFolder() {
+        FeedbackText.text = "";
+        var path = StandaloneFileBrowser.SaveFilePanel("Save File", "", "output " + DateTime.Now + ".png", extensions);
+        if (path.Length == 0) {
+            FeedbackText.text = "Selected path is empty";
+            return;
+        }
+        StartCoroutine(SaveScreenshot(path));
+    }
+
+    IEnumerator SaveScreenshot(string path) {
+        Canvas.SetActive(false);
+
+        yield return new WaitForEndOfFrame();
+        OutputFolderText.text = path;
+        ScreenCapture.CaptureScreenshot(path);
+
+        yield return new WaitForSeconds(0.5f);
+        Canvas.SetActive(true);
+    }
+
+    public void DidTapSelectImageFile()
+    {
+        FeedbackText.text = "";
+        StartCoroutine("selectImage");
+    }
+
+    IEnumerator selectImage() {
+
+        string[] paths = StandaloneFileBrowser.OpenFilePanel("Open File", "", extensions, false);
+        
+        if (paths.Length == 0 || paths[0].Length == 0) {
+            FeedbackText.text = "Selected path is empty";
+            yield break;
+        }
+
+        InputFileText.text = paths[0];
+
+        WWW load = new WWW("file:///" + paths[0]);
+        yield return load;
+        if (!string.IsNullOrEmpty(load.error)) {
+            Debug.LogWarning(paths + " error");
+            FeedbackText.text = "Something went wrong: " + load.error;
+        } else {
+            InputImageTexture = load.texture;
+        }
+    }
+}
---
+++ Assets/Scripts/PaintingManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintingManager : MonoBehaviour
+{
+    public MainManager MainManager;
+    public GameObject PaintContainer;
+    public GameObject CubePrefab;
+
+    Shader lit;
+
+    private void Awake() {
+        lit = Shader.Find("Universal Render Pipeline/Lit");
+    }
+
+    public void Paint() {
+        if (MainManager.InputImageTexture == null) {
+            return;
+        }
+
+        StartCoroutine("DoPainting");
+    }
+
+    IEnumerator DoPainting() {
+        Texture2D tex = MainManager.InputImageTexture;
+
+        int count = 0;
+
+        yield return new WaitForSeconds(0.02f);
+
+        for ( int x = 0; x < tex.width; x++ ) {
+            for ( int y = 0; y < tex.height; y++ ) {                
+                Color color = tex.GetPixel(x, y);
+                spawn(color, x, y);
+
+                count++;
+
+                if (count % 25 == 0) {
+                    yield return new WaitForEndOfFrame();
+                }
+            }
+        }
+    }
+
+    void spawn(Color color, int x, int y) {
+        GameObject newCube = Instantiate(CubePrefab, new Vector3(x, 4f, y), Quaternion.identity);
+
+        Material mat = new Material(lit);
+        mat.color = color;
+        newCube.GetComponent<Renderer>().material = mat;
+
+        newCube.transform.SetParent(PaintContainer.transform);
+    }
+}
